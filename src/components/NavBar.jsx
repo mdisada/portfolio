29ad59be612fa6613b33f63c5ea490f5393719f9
mdisada <mig.isada@gmail.com>
2024-09-09 15:23:43 +0800

    Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
   return (
     <nav className="flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img src={logo} alt="logo"></img>
+        <img className="mx-2 w-10" src={logo} alt="logo"></img>
       </div>
       <div className="m-8 flex column items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
