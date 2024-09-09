66368a5564e8c395ae4d034117f02852a1d543e2
mdisada <mig.isada@gmail.com>
2024-09-09 15:12:55 +0800

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import NavBar from "./components/NavBar";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <NavBar />
+    </div>
+  );
 };
 
 export default App;
